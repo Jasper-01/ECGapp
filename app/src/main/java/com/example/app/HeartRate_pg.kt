a0fdb6fbc574c00525705f2a55c373c82ab3b8f1
Jasper-01 <jasperyap2009@gmail.com>
2022-10-26 18:39:02 +0900

    Calendar and clock added to Heart Rate page

--- app/src/main/java/com/example/app/HeartRate_pg.kt
+++ app/src/main/java/com/example/app/HeartRate_pg.kt
@@ -1,11 +1,16 @@
 package com.example.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextClock
+import androidx.appcompat.app.AppCompatActivity
 
 class HeartRate_pg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_heart_rate_pg)
+        val textClock = findViewById<TextClock>(R.id.Time)
+
+        textClock.format12Hour = null
+        textClock.format24Hour = "yyyy, LLLL dd (E) HH:mm:ss"
     }
 }
