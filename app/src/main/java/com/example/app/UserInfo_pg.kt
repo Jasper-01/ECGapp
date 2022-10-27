74f0bd665c575d348ea7aea41283da663fcb4550
Jasper-01 <jasperyap2009@gmail.com>
2022-10-27 21:13:39 +0900

    appearance for proposal. Changes will be made

--- app/src/main/java/com/example/app/UserInfo_pg.kt
+++ app/src/main/java/com/example/app/UserInfo_pg.kt
@@ -1,11 +1,33 @@
 package com.example.app
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class UserInfo_pg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_info_pg)
+
+        var chgButton = findViewById<Button>(R.id.changeName)
+        var nameDisplay = findViewById<EditText>(R.id.UserNameDisplay)
+        var underline = findViewById<View>(R.id.underlineName)
+        nameDisplay.setEnabled(false)
+
+        chgButton.setOnClickListener {
+            if(!nameDisplay.isEnabled){  // not enable = cannot edit
+                nameDisplay.setEnabled(true)  // can edit text
+                chgButton.setText(R.string.save)
+                underline.setBackgroundColor(Color.DKGRAY)
+            }
+            else{
+                nameDisplay.setEnabled(false)  // cannot edit text
+                chgButton.setText(R.string.change)
+                underline.setBackgroundColor(Color.TRANSPARENT)
+            }
+        }
     }
 }
