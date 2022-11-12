b6ee4a7322c1123ab8747df95895ea13c3d8c80b
Jasper-01 <jasperyap2009@gmail.com>
2022-11-12 22:39:57 +0900

    trying my best

--- MyHeartbeat/src/main/java/com/example/app/History_pg.kt
+++ MyHeartbeat/src/main/java/com/example/app/History_pg.kt
@@ -1,23 +1,71 @@
 package com.example.app
 
-import android.graphics.Color
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.*
 
 class History_pg : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var userArrayList: ArrayList<user>
+    private lateinit var adapter: adapter
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.history_pg)
-        Log.d("MyHeartBeat", "History page create")
-
-        val historyList = findViewById<ListView>(R.id.HistoryDisplay)
-        historyList.setBackgroundColor(Color.TRANSPARENT)
-        val listItems = resources.getStringArray(R.array.sampleHistory)
+        Log.d("MyHeartBeat", "History page created")
+//        val historyList = findViewById<ListView>(R.id.HistoryDisplay)
+//        historyList.setBackgroundColor(Color.TRANSPARENT)
+        FirebaseApp.initializeApp(this)
+        val sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE)
+        val ID_value = sharedPreferences.getString("value", "0").toString()
+        val name = sharedPreferences.getString("name", "0").toString()
+        val date = sharedPreferences.getString("date_time", "0").toString()
+/*        val listItems = resources.getStringArray(R.array.sampleHistory)
         val historyAdapter = ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, listItems)
-        historyList.adapter = historyAdapter
+        historyList.adapter = historyAdapter*/
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.setHasFixedSize(true)
+
+        userArrayList = arrayListOf()
+        adapter = adapter(userArrayList)
+        recyclerView.adapter = adapter
+        EventChangeListener(name, ID_value, date)
+
+    }
+
+    private fun EventChangeListener(name: String, ID_value: String, date: String){
+
+        db = FirebaseFirestore.getInstance()
+        db.collection(name +"_" + ID_value).
+        addSnapshotListener(object: EventListener<QuerySnapshot>{
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onEvent(
+                value: QuerySnapshot?,
+                error: FirebaseFirestoreException?
+            ) {
+                if(error != null) {
+                    Log.e("Firestore Error", error.message.toString())
+                    return
+                }
+                for(dc : DocumentChange in value?.documentChanges!!){
+                    if(dc.type == DocumentChange.Type.ADDED){
+                        userArrayList.add(dc.document.toObject(user::class.java))
+                    }
+                }
+                userArrayList.sortByDescending {
+                    it.datetime
+                }
+                adapter.notifyDataSetChanged()
+            }
+        })
     }
 }
---
+++ MyHeartbeat/src/main/java/com/example/app/user.kt
@@ -0,0 +1,3 @@
+package com.example.app
+
+data class user(var name:String?= null, var datetime: String?=null, var bpm: String?=null)
