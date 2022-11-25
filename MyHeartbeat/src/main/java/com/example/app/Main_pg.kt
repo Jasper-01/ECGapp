0fb51903b1c280fdf5a7bdc85a2176837828911c
Sevete17 <michaelaugusta15@gmail.com>
2022-11-25 10:19:14 +0900

    Login Google Page made

--- MyHeartbeat/src/main/java/com/example/app/Main_pg.kt
+++ MyHeartbeat/src/main/java/com/example/app/Main_pg.kt
@@ -1,5 +1,6 @@
 package com.example.app
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -8,22 +9,41 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Main_pg : AppCompatActivity() {
 
     private var doubleBackToExitPressedOnce = false
+    private lateinit var auth: FirebaseAuth
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MyHeartBeat", "Homepage create")
+        auth = FirebaseAuth.getInstance()
 
+        val email = intent.getStringExtra("email")
+        val display_name = intent.getStringExtra("full_name")
+        Log.d("DKMOBILE", display_name.toString())
+
+        val sharedPref = getSharedPreferences("myKey", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString("email", email.toString())
+        editor.putString("full_name", display_name.toString())
+        editor.apply()
+
+        val sign_out = findViewById<Button>(R.id.sign_out)
         val heartRate = findViewById<Button>(R.id.HeartRate)
         val bluetooth = findViewById<Button>(R.id.BT)
         val friends = findViewById<Button>(R.id.History)
         val settings = findViewById<Button>(R.id.UserInfo)
         val credits = findViewById<Button>(R.id.Credits)
 
+        sign_out.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, login_google_pg::class.java))
+        }
         heartRate.setOnClickListener {
             val Intent = Intent(this, HeartRate_pg::class.java)
             startActivity(Intent)
